2128768703c9e5d00a95fdc26176c1cca91886b0
Vladimir Zoubritsky <vladimir@shiftjs.com>
2019-09-12 16:12:52 +0300

    Do not interactively suggest commands
    Behave similarly to git suggestions

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import {color} from '@oclif/color'
 import {Hook} from '@oclif/config'
-import {cli} from 'cli-ux'
 import * as Levenshtein from 'fast-levenshtein'
 import * as _ from 'lodash'
 
@@ -23,23 +22,9 @@ const hook: Hook.CommandNotFound = async function (opts) {
   }
 
   let suggestion: string = closest(opts.id)
-  this.warn(`${color.yellow(opts.id)} is not a ${opts.config.bin} command.`)
-
-  let response
-  try {
-    response = await cli.prompt(`Did you mean ${color.blueBright(suggestion)}? [y/n]`, {timeout: 4900})
-  } catch (err) {
-    this.log('')
-    this.debug(err)
-  }
-
-  if (response === 'y') {
-    const argv = process.argv
-    await this.config.runCommand(suggestion, argv.slice(3, argv.length))
-    this.exit(0)
-  }
-
-  this.error(`Run ${color.cmd(binHelp)} for a list of available commands.`, {exit: 127})
+  this.error(`${color.yellow(opts.id)} is not a ${opts.config.bin} command.\n` +
+            `Run ${color.cmd(binHelp)} for a list of available commands.\n\n` +
+            `The most similar command is: ${color.blueBright(suggestion)}`, {exit: 127})
 }
 
 export default hook
