954a0a8044ea1bcafa111089ee5eb1b0edb0e807
Vladimir Zoubritsky <vladimir@shiftjs.com>
2019-09-12 16:21:57 +0300

    Publish as a @binaris package

--- test/hooks/not-found.test.ts
+++ test/hooks/not-found.test.ts
@@ -7,8 +7,8 @@ describe('command_not_found', () => {
   test
     .stderr()
     .hook('command_not_found', {id: 'commans'})
-    .catch('commans is not a @oclif/plugin-not-found command.\n' +
-           'Run @oclif/plugin-not-found help for a list of available commands.\n\n' +
+    .catch('commans is not a @binaris/oclif-plugin-not-found command.\n' +
+           'Run @binaris/oclif-plugin-not-found help for a list of available commands.\n\n' +
            'The most similar command is: commands')
     .end('runs hook with not found error')
 })
