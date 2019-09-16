192726061d95f45dc9527d1cf007125fdcc047f5
Vladimir Zoubritsky <vladimir@shiftjs.com>
2019-09-16 14:49:27 +0300

    CR: Add spelling comment

--- test/hooks/not-found.test.ts
+++ test/hooks/not-found.test.ts
@@ -6,7 +6,7 @@ chalk.enabled = false
 describe('command_not_found', () => {
   test
     .stderr()
-    .hook('command_not_found', {id: 'commans'})
+    .hook('command_not_found', {id: 'commans'}) // intentionally misspell "commands"
     .catch('commans is not a @binaris/oclif-plugin-not-found command.\n' +
            'Run @binaris/oclif-plugin-not-found help for a list of available commands.\n\n' +
            'The most similar command is: commands')
